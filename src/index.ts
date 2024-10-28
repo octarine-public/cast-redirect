d5e20f4b779c6f6621f95d3506ecbfa119e3b387
RustiktamQ <rramilperm@gmail.com>
2024-10-28 23:11:04 +0500

    remove other

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,8 @@ import {
 } from "github.com/octarine-public/wrapper/index"
 
 class CCastRedirector {
-	private readonly units: Unit[] = []
-
 	constructor() {
-		EventsSDK.on("EntityCreated", this.onEntityCreated.bind(this))
-		EventsSDK.on("EntityDestroyed", this.onEntityDestroyed.bind(this))
 		EventsSDK.on("TrackingProjectileCreated", this.onTrackProjectile.bind(this))
-	}
-
-	protected onEntityCreated(entity: Entity) {
-		if (this.isIllusion(entity)) {
-			this.units.push(entity)
-		}
-	}
-
-	protected onEntityDestroyed(entity: Entity) {
-		const index = this.units.indexOf(entity as Unit)
-		if (index > -1) {
-			this.units.splice(index, 1)
-		}
 	}
 
 	protected onTrackProjectile(proj: TrackingProjectile) {
@@ -33,7 +16,6 @@ class CCastRedirector {
 			const originalHero = this.getOriginalHero(proj.Target as Unit)
 			if (originalHero) {
 				this.redirectProjectile(proj, originalHero)
-				console.log("Redirecting to hero", originalHero.Index)
 			} else {
 				console.log(":(")
 			}
@@ -51,9 +33,9 @@ class CCastRedirector {
 	private getOriginalHero(illusion: Unit): Hero | null {
 		return illusion.ReplicatingOtherHeroModel as Hero || null;
 	}
+
 	private redirectProjectile(proj: TrackingProjectile, newTarget: Hero) {
-		proj.Target = newTarget
-		console.log(newTarget.Name)
+		
 	}
 }
 
