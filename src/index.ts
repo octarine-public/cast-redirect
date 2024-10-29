76ad19955d512a0ce4ab54dedc8de01016598f9f
RustiktamQ <rramilperm@gmail.com>
2024-10-29 16:39:00 +0500

    check isIllusion

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {
 	Entity,
 	EventsSDK,
+	ExecuteOrder,
 	Hero,
 	Unit
 } from "github.com/octarine-public/wrapper/index"
@@ -12,17 +13,11 @@ class CCastRedirector {
 
 	protected PrepareUnitOrders(order: ExecuteOrder) {
 		console.log(order)
-		// let state = true
-		// for (let i = this.modules.length - 1; i > -1; i--) {
-		// 	const [cModule] = this.modules[i]
-		// 	if (!cModule.State || cModule.PrepareUnitOrders === undefined) {
-		// 		continue
-		// 	}
-		// 	if (cModule.PrepareUnitOrders(order)) {
-		// 		state = false
-		// 	}
-		// }
-		// return state
+		console.log(this.IsIllusion(order))
+	}
+
+	protected IsIllusion(order: ExecuteOrder): boolean {
+		return order.Target?.IsIllusion
 	}
 }
 
