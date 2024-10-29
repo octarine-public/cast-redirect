f45aea460c9e93a9954a9e63954076be48578522
RustiktamQ <rramilperm@gmail.com>
2024-10-29 16:16:44 +0500

    check event PrepareUnitOrders

--- src/index.ts
+++ src/index.ts
@@ -2,90 +2,28 @@ import {
 	Entity,
 	EventsSDK,
 	Hero,
-	Unit,
-	ProjectileManager
+	Unit
 } from "github.com/octarine-public/wrapper/index"
 
 class CCastRedirector {
 	constructor() {
-		EventsSDK.on("TrackingProjectileCreated", this.onTrackProjectile.bind(this))
-		EventsSDK.on("TrackingProjectileUpdated", (proj: TrackingProjectile) => {
-			console.log("projectile update")
-			console.log(proj)
-		})
+		EventsSDK.on("PrepareUnitOrders", this.PrepareUnitOrders.bind(this))
 	}
 
-	protected onTrackProjectile(proj: TrackingProjectile) {
-		if (proj.Source?.Name === "npc_dota_hero_vengefulspirit" && this.illusionIsTarget(proj)) {
-			const originalHero = this.getOriginalHero(proj.Target as Unit)
-			if (originalHero) {
-				console.log("projectile")
-				console.log(proj)
-
-				console.log("original hero")
-				console.log(originalHero)
-
-				console.log("illusion")
-				console.log(proj.Target)
-
-				this.redirectProjectile(proj, originalHero)
-
-				console.log("updated projectile")
-				console.log(proj)
-
-				this.destroyProjectile(proj)
-			} else {
-				console.log(":(")
-			}
-		}
-	}	
-
-	private destroyProjectile(proj: TrackingProjectile) {
-		ProjectileManager.AllTrackingProjectiles.remove(proj)
-		ProjectileManager.AllTrackingProjectilesMap.delete(proj.ID)
-		proj.IsValid = false
+	protected PrepareUnitOrders(order: ExecuteOrder) {
+		console.log(order)
+		// let state = true
+		// for (let i = this.modules.length - 1; i > -1; i--) {
+		// 	const [cModule] = this.modules[i]
+		// 	if (!cModule.State || cModule.PrepareUnitOrders === undefined) {
+		// 		continue
+		// 	}
+		// 	if (cModule.PrepareUnitOrders(order)) {
+		// 		state = false
+		// 	}
+		// }
+		// return state
 	}
-
-	private illusionIsTarget(proj: TrackingProjectile) {
-		return proj.Target.IsIllusion_
-	}
-
-	private getOriginalHero(illusion: Unit): Hero | null {
-		return illusion.ReplicatingOtherHeroModel as Hero || null;
-	}
-
-	private redirectProjectile(proj: TrackingProjectile, newTarget: Hero) {
-		proj.Update(
-			newTarget, 
-			proj.Speed, 
-			proj.ParticlePath, 
-			proj.ParticleSystemHandle, 
-			proj.dodgeable, 
-			proj.isAttack, 
-			proj.expireTime, 
-			proj.LaunchTick,
-			newTarget.NetworkedPosition
-		)
-	}
-
-	private redirectProjectile2(proj: TrackingProjectile, newTarget: Unit) {
-		// Удаляем текущий прожектайл
-		DestroyTrackingProjectile(proj);
-	
-		// Создаем новый прожектайл с измененной целью
-		const newProjectile = {
-			Source: proj.Source,
-			Target: newTarget,
-			Speed: proj.Speed,
-			LaunchTick: GameState.CurrentGameTick,
-			ParticlePath: proj.ParticlePath,
-			dodgeable: proj.dodgeable,
-			isAttack: proj.isAttack,
-			expireTime: proj.expireTime,
-		};
-		TrackingProjectileCreated(newProjectile as TrackingProjectile);
-	}
-	
 }
 
 const castRedirector: CCastRedirector = new CCastRedirector()
