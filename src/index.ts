09494f5bd2523aefff6995765f5dc84a4f0f2f96
RustiktamQ <rramilperm@gmail.com>
2024-10-30 19:41:54 +0500

    clean

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import {
 	EntityManager,
 	Unit,
 	Ability,
-	Hero
+	Hero,
+	Creep,
+	FakeUnit
 } from "github.com/octarine-public/wrapper/index"
 
 class CCastRedirector {
@@ -14,41 +16,53 @@ class CCastRedirector {
 	}
 
 	protected PrepareUnitOrders(order: ExecuteOrder): boolean {
-		if (order.IsPlayerInput && this.IsAbility(order) && this.IsToTarget(order.Target)) {
-			if (order?.Target?.IsIllusion) {
-				const newTarget = this.GetOriginalHero(order.Target)
+		if (order.IsPlayerInput && this.IsAbility(order.Ability_) && this.IsToTarget(order.Target)) {
+			if (this.IsIllusion(order.Target)) {
+				const newTarget = this.GetOriginalHero(order.Target) as Unit
 				const caster = order.Issuers[0]
+				const ability = order.Ability_ as Ability
 
-				if (this.IsAvailableOriginalHero(newTarget, caster)) {
-					caster.CastTarget(order.Ability_, newTarget)
+				if (this.IsAvailableOriginalHero(newTarget, caster) && this.IsAbility(order.Ability_)) {
+					caster.CastTarget(ability, newTarget)
 				} else {
 					const nearliestHero = this.GetNearestOtherHero(newTarget, caster)
 
 					if (!nearliestHero) return true
-					caster.CastTarget(order.Ability_, nearliestHero)
+					caster.CastTarget(ability, nearliestHero)
 				}
 
 				return false
-			} else {
+			} else if (this.IsCreep(order.Target)){
 				console.log(order.Target)
 			}
 		}
 		return true
 	}
 
-	protected IsAbility(order: ExecuteOrder): boolean {
-		return order.Ability_ instanceof Ability
+	private IsIllusion(target: Nullable<Entity  | number>): boolean {
+		if (target instanceof Unit) {
+			return (target.IsIllusion || target.IsHiddenIllusion) && !target.IsStrongIllusion
+		}
+		return false
 	}
 
-	protected IsToTarget(target: Entity): boolean {
+	protected IsCreep(target: Nullable<number | Entity>): boolean {
+		return target instanceof Creep
+	}
+
+	protected IsAbility(ability: Nullable<number | Ability>): boolean {
+		return ability instanceof Ability
+	}
+
+	protected IsToTarget(target: Nullable<Entity | number>): boolean {
 		return target instanceof Unit
 	}
 
-	protected GetOriginalHero(target: Entity): Entity {
-		return target.ReplicatingOtherHeroModel		
+	protected GetOriginalHero(target: Nullable<number | Entity>): Nullable<Unit | FakeUnit> {
+		if (target instanceof Hero) return target.ReplicatingOtherHeroModel;
 	}
 
-	protected GetNearestOtherHero(target: Entity, caster: Unit): Nullable<Entity | undefined> {
+	protected GetNearestOtherHero(target: Entity, caster: Unit): Nullable<Unit> {
 		return EntityManager.GetEntitiesByClass(Hero)
 		.sort((a, b) => a.Distance2D(caster) - b.Distance2D(caster))
 		.find( x =>
@@ -56,13 +70,14 @@ class CCastRedirector {
 			x !== caster &&
 			!x.IsIllusion &&
 			!x.IsInvulnerable &&
-			x.Distance2D(caster) < 1800 &&
+			x.Distance2D(caster) < 1200 &&
 			x.IsEnemy(caster)
 		)
 	}
 
-	protected IsAvailableOriginalHero(hero: Entity, caster: Unit): boolean {
-		return hero.IsVisible && hero.IsAlive && hero.Distance2D(caster) < 1800;
+	protected IsAvailableOriginalHero(hero: Nullable<Unit | FakeUnit>, caster: Unit): boolean {
+		if (hero instanceof Unit) return hero.IsVisible && hero.IsAlive && hero.Distance2D(caster) < 1800;
+		return false
 	}
 
 	// to do redirect from creeps
