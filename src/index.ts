59add792b869df733379687b63a9668dfbf8f708
RustiktamQ <rramilperm@gmail.com>
2024-10-30 23:04:12 +0500

    log#2

--- src/index.ts
+++ src/index.ts
@@ -94,6 +94,7 @@ new (class CCastRedirector {
 	}
 
 	protected GetNearestOtherHero(target: Entity, caster: Unit): Nullable<Unit> {
+		console.log(EntityManager.GetEntitiesByClass(Hero))
 		return EntityManager.GetEntitiesByClass(Hero)
 		.sort((a, b) => a.Distance2D(caster) - b.Distance2D(caster))
 		.find( x =>
